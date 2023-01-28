2d98113626c00196d6f3c5436d55ad29c2d7ffc4
Mike <muzika777@yandex.ru>
2023-01-28 22:07:27 +0400

    firstStepJs

---
+++ js/script.js
@@ -0,0 +1,85 @@
+
+/* let number = 5;
+const leftBorderWidth = 1;
+console.log(number);
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+var name = 'Mike';
+ */
+
+/* const obj = {               
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.age)
+
+let arr = ['plum.png', 'orange.jpg', 6]
+
+console.log(arr[0]) // вызов значения по ключу  */
+
+"use strict";
+
+                                    // Простое общение с пользователем 
+
+/* const answer = prompt('Вам есть 18?', '18');
+console.log(typeof(answer)); */
+
+/* const answer = [];
+
+answer[0] = prompt('Напишите выше имя?', '');
+answer[1] = prompt('Какая у Вас фамилия?', '');
+answer[2] = prompt('Сколько вам лет?', '');
+
+document.write(answer); */
+
+                                            // Интерполяция 
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Mike';
+
+alert(`Привет, ${user}`);
+  */
+
+                                            // Операторы 
+
+console.log('arr' + '- object');
+console.log(4 + + '- object');
+
+let incr = 10,
+    decr = 10;
+
+/* ++incr;
+--decr; */
+
+console.log(++incr);
+console.log(--decr);
+
+// == - сравнение 
+// === - строгое сравнение 
+// && - И два правдивы
+// || - ИЛИ один правдив
+// ! - оператор отрицания  
+//
+
+const isChecked = true
+
+// 
+
+/* let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+console.log(numberOfFilms) */
