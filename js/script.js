0bb68a43c78a7140f9da806d7416176b2b88ec10
Mike <muzika777@yandex.ru>
2023-01-31 19:50:15 +0400

    independent work 018

--- js/script.js
+++ js/script.js
@@ -129,7 +129,7 @@ console.log(numberOfFilms) */
 
 //                                   Циклы
 
-let num = 50;
+// let num = 50;
 
 // while (num < 55) {
 //     console.log(num);
@@ -153,41 +153,192 @@ let num = 50;
 //                         Самостоятельная работа 015
 
 // 1)
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false 
+// }; 
+
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done')
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+    
+// }
+
+// if (personalMovieDB.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     console.log("Вы киноман");
+// } else {
+//     console.log('Произошла ошибка');
+// }
+
+// console.log(personalMovieDB);
+
+
+//                               Стрелочные функции
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 10;
+// }
+
+// showFirstMessage('Hello World');
+// console.log(num);
+
+// function calc(a, b) {
+//     return(a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {            //  FUNCTION DECLARATION      
+//     let num = 50;           // Создаётся до начала выполнения скрипта, 
+//                             //   можно    вызвать перед обьявлением
+
+//     //
+
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// let logger = function () {           //  FUNCTION EXPRESSION 
+//     console.log('hello world');      // Создаётся только тогда, когда доходит кода,можно вызвать только после объявления
+// };
+
+// logger();
+
+// const calc = (a, b) => {return a + b};       // Не имеет своего контекста вызова (this)
+
+// const calc = (a, b) => {
+//     console.log('1');
+//     return a + b;
+// }; 
+
+
+//                                   Методы и свойства строк и чисел 
+
+// const str = 'teSt';
+// const arr = [1, 5, 7];
+
+// console.log(str.toUpperCase()); // Все значения в верхнем регистре 
+// console.log(str.toLowerCase()); // все значения в нижнем регистре 
+
+// let fruit = "Some fruit";
+
+// console.log(fruit.indexOf("q")); //  Выведет значение -1, что означает отсутствие в строчке такого значения.
+
+// const logg = "Hello World";
+
+// //console.log(logg.slice(0, 5)); //  Возвращает новый массив содержащий извлеченные элементы, поддерживает отрицательные значения  
+
+// //console.log(logg.substring(0, 5)); // Как slice() только не поддерживает отрицательные значения 
+
+// console.log(logg.substr(6, 2)); //  Метод начинает извлекать символы начиная с указанной позиции. И второй аргумент указывает на длину, колво символов
+
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+//             Самостоятелььная работа 018
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+const personalMovieDB = {               // база данных
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false 
+    privat: false
 }; 
 
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-    
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done')
-    } else {
-        console.log('error');
-        i--;
+
+
+function remembermyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else {
+            console.log('error');
+            i--;
+        }            
     }
-
-    
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log('Произошла ошибка');
+remembermyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
 
+//console.log(personalMovieDB);
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
