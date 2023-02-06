ba36cb4e1564a1ac56cbb4bb5a8be0f317e61e95
Mike <muzika777@yandex.ru>
2023-02-06 17:58:30 +0400

    024 Практика ч.4 Используем объекты

---
+++ js/024 script1.js
@@ -0,0 +1,89 @@
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку.  бб тм 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+const personalMovieDB = {               // база данных
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
+
+
+
+
+function remembermyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done')
+        } else {
+            console.log('error');
+            i--;
+        }            
+    }
+}
+
+remembermyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+detectPersonalLevel();
+
+//console.log(personalMovieDB);
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
--- js/script.js
+++ js/script.js
@@ -447,84 +447,206 @@ console.log(numberOfFilms) */
 
 //                              Передача по ссылке или по значению
 
-let a = 5, 
-    b = a;
+// let a = 5, 
+//     b = a;
 
-b = b + 5;
+// b = b + 5;
 
-console.log(b);
-console.log(a);
+// console.log(b);
+// console.log(a);
 
-const obj = {
-    a: 5,
-    b: 1
-};
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
 
-const copy = obj; // Ссылка.. нащывается передача по ссылке
+// const copy = obj; // Ссылка.. нащывается передача по ссылке
 
-copy.a = 10;
+// copy.a = 10;
 
-console.log(copy);
-console.log(obj);
+// console.log(copy);
+// console.log(obj);
 
-function copy(mainObj) {
-    let objCopy = {};
+// function copy(mainObj) {
+//     let objCopy = {};
 
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+//    return objCopy;
+// }       
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7, 
+//         y: 4
+//     }  
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// }
+
+//  const clone = Object.assign({}, add); // Объединение объектов
+
+//  clone.d = 20;
+
+// console.log(clone);
+// console.log(add);
+
+
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'sdsdfsdf';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+// //                       Четвертый способ создания поверхостных копий 
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//     blogs = ['wordpress', 'lifejournal', 'blogger'],
+//     internet = [...video, ...blogs, 'vk', 'facebook']; // Оператор разворота ...
+
+//     console.log(internet);
+
+//     function log (a, b, c) {
+//         console.log(a);
+//         console.log(b);
+//         console.log(c);
+//     }
+
+//     const num = [2, 5, 7];
+
+//     log(...num);
+
+//     const array = ['a', 'b'];
+
+//     const newAarray = [...array];
+
+//     const q = {
+//         one: 1,
+//         two: 2
+//     };
+
+//     const newObj = {...q};
+
+//     console.log(newObj);
+
+//              Основы ООП, прототипно-ориентированное наследование 
+
+// let str = 'some';
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1, 2, 3]);
+
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function () {
+//         console.log('Hello');
+//     }
+// };
+
+// const jonh = Object.create(soldier);  Один из вариантов прототипа
+
+// const jonh = {
+//     health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+//Object.setPrototypeOf(jonh, soldier);    Правильный тип прототипирования 
+
+//console.log(jonh.armor);
+
+// jonh.sayHello();
+
+//                           024 Практика ч.4 Используем объекты
+
+
+const personalMovieDB = {               // база данных
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    remembermyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+            
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('done')
+            } else {
+                console.log('error');
+                i--;
+            }            
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log('Произошла ошибка');
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if(genre == '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i+1} - это ${item}`);
+        })
     }
-   return objCopy;
-}       
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7, 
-        y: 4
-    }  
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-console.log(newNumbers);
-console.log(numbers);
-
-const add = {
-    d: 17,
-    e: 20
-}
-
- const clone = Object.assign({}, add); // Объединение объектов
-
- clone.d = 20;
-
-console.log(clone);
-console.log(add);
+}; 
 
 
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'sdsdfsdf';
-
-console.log(newArray);
-console.log(oldArray);
-
-//                       Четвертый способ создания поверхостных копий 
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'lifejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook']; // Оператор разворота ...
-
-    console.log(internet);
-
-    function log (a, b, c) {
-        console.log(a);
-        console.log(b);
-        console.log(c);
-    }
