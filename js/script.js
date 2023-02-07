a6e50763bf5d9f30c8b2208f603e99af66fa4cb7
Mike <muzika777@yandex.ru>
2023-02-07 18:12:54 +0400

    026 Динамическая типизация

--- js/script.js
+++ js/script.js
@@ -581,72 +581,100 @@ console.log(numberOfFilms) */
 //                           024 Практика ч.4 Используем объекты
 
 
-const personalMovieDB = {               // база данных
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB = {               // база данных
+//     count: 0,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+//     start: function() {
+//         personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
     
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-        }
-    },
-    remembermyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последних фильмов?', ''),
-                  b = prompt('На сколько оцените его?', '');
+//         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+//             personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//         }
+//     },
+//     remembermyFilms: function() {
+//         for (let i = 0; i < 2; i++) {
+//             const a = prompt('Один из последних фильмов?', ''),
+//                   b = prompt('На сколько оцените его?', '');
             
-            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-                console.log('done')
-            } else {
-                console.log('error');
-                i--;
-            }            
-        }
-    },
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-            console.log("Просмотрено довольно мало фильмов");
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log("Вы классический зритель");
-        } else if (personalMovieDB.count >= 30) {
-            console.log("Вы киноман");
-        } else {
-            console.log('Произошла ошибка');
-        }
-    },
-    showMyDB: function(hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//             if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//                 personalMovieDB.movies[a] = b;
+//                 console.log('done')
+//             } else {
+//                 console.log('error');
+//                 i--;
+//             }            
+//         }
+//     },
+//     detectPersonalLevel: function() {
+//         if (personalMovieDB.count < 10) {
+//             console.log("Просмотрено довольно мало фильмов");
+//         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//             console.log("Вы классический зритель");
+//         } else if (personalMovieDB.count >= 30) {
+//             console.log("Вы киноман");
+//         } else {
+//             console.log('Произошла ошибка');
+//         }
+//     },
+//     showMyDB: function(hidden) {
+//         if (!hidden) {
+//             console.log(personalMovieDB);
+//         }
+//     },
+//     toggleVisibleMyDB: function() {
+//         if (personalMovieDB.privat) {
+//             personalMovieDB.privat = false;
+//         } else {
+//             personalMovieDB.privat = true;
+//         }
+//     },
+//     writeYourGenres: function() {
+//         for (let i = 1; i <= 3; i++) {
+//             let genre = prompt(`Ваш любимый жанр под номером ${i}`);
 
-            if(genre == '' || genre == null) {
-                console.log('Вы ввели некорректные данные или не ввели их вовсе');
-                i--;
-            } else {
-                personalMovieDB.genres[i - 1] = genre;
-            }
-        }
+//             if(genre == '' || genre == null) {
+//                 console.log('Вы ввели некорректные данные или не ввели их вовсе');
+//                 i--;
+//             } else {
+//                 personalMovieDB.genres[i - 1] = genre;
+//             }
+//         }
 
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Любимый жанр ${i+1} - это ${item}`);
-        })
-    }
-}; 
+//         personalMovieDB.genres.forEach((item, i) => {
+//             console.log(`Любимый жанр ${i+1} - это ${item}`);
+//         })
+//     }
+// }; 
 
+
+
+
+
+//                             025  Отлавливаем ошибки в своём коде при помощи консоли разработчика. Breakpoints
+//                             026 Динамическая типизация 
+
+//1)
+
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2)                         
+
+console.log(typeof(5 + ''));  // Конкатенация- это сложение строк/строки с чем- то 
+
+let gg = 5;
+
+console.log("https://vk.com/catalog/" + gg);
+
+
+
+//3)
+
+
+
+
+ 
 
